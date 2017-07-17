997176dcb6fde373325443b856948fb6ef05f612
andriilguvi <andriilguvi68@gmail.com>
2017-07-17 15:11:55 +0530

    Create revstr.java

---
+++ revstr.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class revstr
+{
+public static void main(String args[])
+{
+String str="andriil";
+String reverse=new StringBuffer(str).reverse().toString();
+System.out.print(reverse);
+}
+}
